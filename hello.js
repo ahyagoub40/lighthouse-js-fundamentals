5d4ce98cdbe61f8bd4eb53b84d607bdd56d94958
ahyagoub40 <ahyagoub40@gmail.com>
2019-12-17 23:45:00 +0000

    sayHello function with difference btwn console.log and return

--- hello.js
+++ hello.js
@@ -5,3 +5,14 @@ const sayHello = function (name){ // function with parameter name that displays 
 sayHello("Caliban");  // calling the function with an argument 
 sayHello("Miranda");  // calling the function with an argument 
 sayHello("Ferdinand");  // calling the function with an argument 
+
+let sayHelloToConsole = function (name){
+    console.log("Hello, "+ name);
+}
+sayHelloToConsole("John");  
+
+sayHelloToConsole = function (name){
+    return "Hello, "+ name;  // return doesn't output anything to the console. 
+}
+const greeting = sayHelloToConsole("John");  // the value returned by the console assigned to a const
+console.log(greeting); // display the value of the const
