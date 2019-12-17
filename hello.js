057f0701bce4a82f35d9cce1ff1fd2fddca8911d
ahyagoub40 <ahyagoub40@gmail.com>
2019-12-17 23:30:57 +0000

    Created a sayHello function

---
+++ hello.js
@@ -0,0 +1,7 @@
+const sayHello = function (name){ // function with parameter name that displays the following message to console
+    console.log("Hello, " + name);
+}
+
+sayHello("Caliban");  // calling the function with an argument 
+sayHello("Miranda");  // calling the function with an argument 
+sayHello("Ferdinand");  // calling the function with an argument 
