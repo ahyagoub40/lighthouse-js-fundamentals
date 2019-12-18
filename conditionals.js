ec5153eef09b5f9b7de817a6a937fe89624233e0
ahyagoub40 <ahyagoub40@gmail.com>
2019-12-18 00:04:57 +0000

    is weather raining

--- conditionals.js
+++ conditionals.js
@@ -1,18 +1,5 @@
+const raining = true;  // it is raining
 
-const tempreture = -12;
-
-if (tempreture <0){   // if it is cold, do this
-    console.log("Make sure you pick out a scarf!");
+if (!raining){   // not the value of var raining, opposite to it
+    console.log("Leave your umbrella at home!");
 }
-
-else if (tempreture <15){ // temp between 0 & 15
-    console.log("Short sleeves won't cut it!");
-
-}
-else{
-    // otherwise
-    console.log("Short sleeves are fine.");
-}
-// then 
-console.log("Now you are ready to go outside!");
- 
