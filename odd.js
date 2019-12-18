3319eafbe25c95a9ca265ff20c3a6e76e2cdcf52
ahyagoub40 <ahyagoub40@gmail.com>
2019-12-18 00:15:37 +0000

    check if num is odd

---
+++ odd.js
@@ -0,0 +1,6 @@
+function isOdd(num){  // function that accepts a parameter 
+return num % 2 === 1; // returns true if the number is odd
+}
+// call the function and display the results in a string format
+console.log("3 is odd: " + isOdd(3));  
+console.log("8 is odd: " + isOdd(8));
