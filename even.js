a7d0d45656cf36f2e6db4db32740593c12cc5517
ahyagoub40 <ahyagoub40@gmail.com>
2019-12-18 00:00:40 +0000

    function that tests if the number is even

---
+++ even.js
@@ -0,0 +1,7 @@
+const isEven = function (num){  // function that takes a number 
+    return num % 2 ===0;  // it returns if the numbers is divisible by 2 (true / false)
+}
+
+// call the function and display the value to the console 
+console.log(isEven(10));       
+console.log(isEven(11));
