07ccd5533c7510f940d22ca85f7510ed5e316168
ahyagoub40 <ahyagoub40@gmail.com>
2019-12-19 14:05:41 +0000

    all the ice cream flavours at lighthouse labs

---
+++ iceCreamFlavours.js
@@ -0,0 +1,7 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+iceCreamFlavours.push("root beer"); // add a new flavour to the end of the array
+console.log(iceCreamFlavours); // display the updated array
+console.log(iceCreamFlavours[0]); // display the first flavour in the array
+console.log(iceCreamFlavours[5]); // display the last flavour in the array
+console.log(iceCreamFlavours.length); // display the number of flavours in the array
