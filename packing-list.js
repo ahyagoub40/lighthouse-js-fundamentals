5fef1a717a650d1a8b9b57dd4aadab87461e38cc
ahyagoub40 <ahyagoub40@gmail.com>
2019-12-19 14:06:43 +0000

    stuff to pack

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+// an array for stuff to pack
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+// display this message to the console
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++){ // iterate through every item in the array
+    console.log(packingList[i]); // display the item at the specified index
+}
